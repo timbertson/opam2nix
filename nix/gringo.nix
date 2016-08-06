b2b554ef3e42cd367922d2ba910966a2ea2bbc98
Tim Cuthbertson <tim@gfxmonk.net>
2016-08-06 21:40:42 +1000

    patch gringo

--- nix/gringo.nix
+++ nix/gringo.nix
@@ -8,6 +8,7 @@ stdenv.mkDerivation rec {
     url = "https://sourceforge.net/projects/potassco/files/gringo/${version}/gringo-${version}-source.tar.gz";
     sha256 = "16k4pkwyr2mh5w8j91vhxh9aff7f4y31npwf09w6f8q63fxvpy41";
   };
+  patches = [ ./gringo.patch ];
 
   buildInputs = [ scons bison re2c ];
   buildPhase = ''
