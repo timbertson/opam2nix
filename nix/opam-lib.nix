38d396a731a249b0a4f564ae9134351b5c21da8f
Tim Cuthbertson <tim@gfxmonk.net>
2017-04-01 17:16:29 +1100

    patch opam-lib with ocaml/opam/pull/2897

--- nix/opam-lib.nix
+++ nix/opam-lib.nix
@@ -5,6 +5,10 @@ stdenv.mkDerivation {
 		url = "http://github.com/ocaml/opam/archive/1.3.0.tar.gz";
 		sha256 = "0yhgy6ap6bsj1gclgazjn98sizja9qmb2fgfvk102i7l80li4l4x";
 	};
+
+	# https://github.com/ocaml/opam/pull/2897
+	patches = [ ./opam-url-fix.diff ];
+
 	buildInputs = [ocaml findlib ocaml_extlib cmdliner cudf curl dose3 jsonm ocamlgraph re ];
 	installPhase = "make libinstall";
 	createFindlibDestdir = true;
