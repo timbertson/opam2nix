ccd6f4d4f8afc1f515fce373c50df721d5a86a6c
Tim Cuthbertson <tim@gfxmonk.net>
2017-10-24 18:35:40 +1100

    bump opam-lib

--- nix/opam-lib.nix
+++ nix/opam-lib.nix
@@ -1,9 +1,9 @@
 {stdenv, fetchurl, ocaml, findlib, ocaml_extlib, cmdliner, cudf, curl, dose3, jsonm, ocamlgraph, re }:
 stdenv.mkDerivation {
-	name = "opam-lib";
+	name = "opam-lib-1.3.1";
 	src = fetchurl {
-		url = "http://github.com/ocaml/opam/archive/1.3.0.tar.gz";
-		sha256 = "0yhgy6ap6bsj1gclgazjn98sizja9qmb2fgfvk102i7l80li4l4x";
+		url = "http://github.com/ocaml/opam/archive/1.3.1.tar.gz";
+		sha256 = "1zg92zzzp4nvrw0170mhdml336r7fjf49ldak63wq6aj8v1nfmv2";
 	};
 
 	# https://github.com/ocaml/opam/pull/2897
