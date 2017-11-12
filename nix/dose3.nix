c44a3e00c2af96a08c1a829bfbd543dbc47c3a90
Tim Cuthbertson <tim@gfxmonk.net>
2017-11-12 16:04:15 +1100

    add ocamlbuild to dose

--- nix/dose3.nix
+++ nix/dose3.nix
@@ -1,11 +1,11 @@
-{stdenv, fetchurl, ocaml, findlib, camlp4, ocaml_extlib, cudf, ocamlgraph, cppo, re, perl }:
+{stdenv, fetchurl, ocaml, findlib, ocamlbuild, camlp4, ocaml_extlib, cudf, ocamlgraph, cppo, re, perl }:
 stdenv.mkDerivation {
 	name = "dose";
 	src = fetchurl {
 		url = "http://gforge.inria.fr/frs/download.php/file/36063/dose3-5.0.1.tar.gz";
 		sha256 = "00yvyfm4j423zqndvgc1ycnmiffaa2l9ab40cyg23pf51qmzk2jm";
 	};
-	buildInputs = [ocaml findlib camlp4 ocaml_extlib cudf ocamlgraph cppo re perl ];
+	buildInputs = [ocaml findlib ocamlbuild camlp4 ocaml_extlib cudf ocamlgraph cppo re perl ];
 	# configureFlags = "--with-ocamlgraph";
 	createFindlibDestdir = true;
 	patchPhase = ''
