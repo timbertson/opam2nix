36f0014e9e2981b9da13db3b715c11e1da79c649
Tim Cuthbertson <tim@gfxmonk.net>
2019-06-16 14:19:36 +1000

    fix mccs dependency path

--- nix/mccs.nix
+++ nix/mccs.nix
@@ -1,19 +1,12 @@
-{stdenv, fetchurl, ocaml, dune, findlib, cudf, ocaml_extlib }:
-stdenv.mkDerivation {
-	name = "mccs";
+{ocamlPackages, stdenv, fetchurl, ocaml, dune, findlib, cudf, ocaml_extlib }:
+ocamlPackages.buildDunePackage rec {
+	pname = "mccs";
+	version = "1.1+9";
 	src = fetchurl {
-		url = "https://github.com/AltGr/ocaml-mccs/archive/1.1+9.tar.gz";
+		url = "https://github.com/AltGr/ocaml-mccs/archive/${version}.tar.gz";
 		sha256 = "0gf86c65jdxxcwd96kcmrqxrmnnzc0570gb9ad6c57rl3fyy8yhv";
 	};
 	buildInputs = [ findlib ocaml dune cudf ocaml_extlib ];
 	configurePhase = "true";
-	buildPhase = ''
-		dune build -p mccs
-	'';
-	installPhase = ''
-		mkdir -p $out
-		dune install --prefix $out
-	'';
-	createFindlibDestdir = true;
 }
 
