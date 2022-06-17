5b314ec6c2de67638bbd40c0bbf912037a4e7e8c
Tim Cuthbertson <tim@gfxmonk.net>
2022-06-17 21:03:04 +1000

    fix weird opam-file-format build error

--- nix/default.nix
+++ nix/default.nix
@@ -51,7 +51,14 @@ let
 				src = opam-file-format;
 				propagatedBuildInputs = [ocaml findlib];
 				createFindlibDestdir = true;
-				installPhase = "make install PREFIX=$out LIBDIR=$out/lib/ocaml/${ocaml.version}/site-lib/";
+
+				# META file gets created halfway through the build without native archive,
+				# because those haven't been built yet. And then it doesn't get properly
+				# rebuilt because its deps aren't properly specified
+				installPhase = ''
+					touch src/META.in; make -C src META
+					make install PREFIX=$out LIBDIR=$out/lib/ocaml/${ocaml.version}/site-lib/
+				'';
 			}
 		) {};
 
